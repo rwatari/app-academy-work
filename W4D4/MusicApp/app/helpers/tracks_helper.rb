d28e8c1923f3d809973ba18c24273f23a6402bac
Raku Watari <rwatari@gmail.com>
2016-12-08 17:00:42 -0800

    Add ugly lyrics helper to display lyrics

--- W4D4/MusicApp/app/helpers/tracks_helper.rb
+++ W4D4/MusicApp/app/helpers/tracks_helper.rb
@@ -1,2 +1,9 @@
 module TracksHelper
+  include ERB::Util
+
+  def ugly_lyrics(lyrics)
+    lines = lyrics.strip.split("\r\n")
+    lines.map! { |line| "&#9835; " + h(line) }
+    "<pre>#{lines.join("\n")}</pre>".html_safe
+  end
 end
